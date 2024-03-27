626bc08972ec56ce2a3e3b8e820490b2fa701ba0
Naoyuki Hirata <naoyuki.hirata.biz@gmail.com>
2024-03-27 12:56:36 +0900

    [add]Part 3 - Advanced Concepts

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,14 @@ from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('boards/<int:pk>/', views.board_topics, name='board_topics'),
+    path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
+    # path('about/', views.about, name='about'),
+    # path('about/company/', views.about_company, name='about_company'),
+    # path('about/author/', views.about_author, name='about_author'),
+    # path('about/author/vitor/', views.about_vitor, name='about_vitor'),
+    # path('about/author/erica/', views.about_erica, name='about_erica'),
+    # path('privacy/', views.privacy_policy, name='privacy_policy'),
+    # path('<slug:username>/', views.user_profile, name='user_profile'),
     path('', views.home, name='home'),
 ]
